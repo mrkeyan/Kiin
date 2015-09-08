c9c3ce03c4d79ef5b74ce3e71af157157b9bbfae
Keyan <mrkeyan@hotmail.com>
2015-09-08 19:40:55 -0400

    one page updates

--- scripts/script_onepage.js
+++ scripts/script_onepage.js
@@ -1,15 +1,43 @@
+//if icon 1 or 2 are cliccked, remove the selected class from all
+//add the selected class to the icon clicked
+//animate this transition
 $( "#icon-image-1, #icon-image-2" ).on("click", function(){
     var img_src = $(this).attr("data-src");
     $( ".icon-image").each(function(){
         $(this).removeClass("selected");
+        $(this).addClass("icon-shadow-selected");
     });
     $(this).addClass("selected");
     $( "#main-image" ).attr("src",img_src);
+    if($(this).hasClass("selected")){
+        $(this).removeClass("icon-shadow-selected");
+    }else{
+           
+    }
     
     
 });
 
+//change the zoomed in image to the pointer's data-zoom src
+//will want to animate this to smooth out the transition
 $( "#pointer-1" ).on("click", function(){
     var zoom_src = $( ".selected" ).attr("data-zoom");
+    var img_num = $( ".selected" ).attr("data-num");
     $( "#zoomed_selection" ).attr("src", zoom_src);
+    
+    var prod_info = $( "#product-info-text" );
+    $(".info-image").each(function(){
+       $(this).removeClass("selected-text");
+        $(this).addClass("hidden-text");
+    });
+    if(img_num == "info-image-1"){
+        $(".info-image-1").addClass("selected-text");
+        $(".info-image-1").removeClass("hidden-text");
+    }else if(img_num =="info-image-2"){
+        $(".info-image-2").addClass("selected-text");
+        $(".info-image-2").removeClass("hidden-text");
+    }else{
+        $(".info-image-0").addClass("selected-text");
+        $(".info-image-0").removeClass("hidden-text");
+    }
 });
